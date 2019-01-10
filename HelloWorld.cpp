acf81e575d07329eba531f06a3b2035ce9a79bb8
Sean Vovan <svovan@usc.edu>
2019-01-10 15:32:33 -0800

    My first HelloWorld using git

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main ()
+{
+	std::cout << "HelloWorld!" << std::endl;
+	std::cout << "In order to actually use the Sublime Text Editor change stuff to C++" << std::endl;
+	
+
+}
