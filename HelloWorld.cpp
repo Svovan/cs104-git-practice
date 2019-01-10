95e53e57c723a87ae5fcf033babf9d1557dfe734
Sean Vovan <svovan@usc.edu>
2019-01-10 15:36:06 -0800

    School Pride

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,7 +3,7 @@
 
 int main ()
 {
-	std::cout << "HelloWorld!" << std::endl;
+	std::cout << "Fight on!" << std::endl;
 	std::cout << "In order to actually use the Sublime Text Editor change stuff to C++" << std::endl;
 	
 
